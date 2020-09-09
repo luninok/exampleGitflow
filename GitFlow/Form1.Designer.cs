01b7c2c95f7a48410761fadbb220e58887aa01f8
luninok <luninok@list.ru>
2020-09-10 00:14:57 +0700

    Second hotfix

--- GitFlow/Form1.Designer.cs
+++ GitFlow/Form1.Designer.cs
@@ -44,12 +44,13 @@
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.SystemColors.Highlight;
             this.button2.Location = new System.Drawing.Point(171, 138);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(119, 23);
             this.button2.TabIndex = 1;
             this.button2.Text = "SecondClick";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
