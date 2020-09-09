e91c829aa8a468e642c87abb5f67ffab1abe1b97
luninok <luninok@list.ru>
2020-09-09 23:15:17 +0700

    Add first feature

--- GitFlow/Form1.cs
+++ GitFlow/Form1.cs
@@ -16,5 +16,10 @@ namespace GitFlow
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("First feature!");
+        }
     }
 }
